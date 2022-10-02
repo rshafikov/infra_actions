028fcd37309535ec144ae4451a5f2adbc3b73abf
Ramil Shafikov <rshafikov.work@yandex.ru>
2022-10-02 15:17:16 +0400

    tests has been fixed

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
 
 from django.test import Client, TestCase
-
+from django.urls import reverse
 
 class StaticPagesURLTests(TestCase):
     def setUp(self):
@@ -9,16 +9,16 @@ class StaticPagesURLTests(TestCase):
 
     def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
-        response = self.guest_client.get('/')
+        response = self.guest_client.get(reverse('index'))
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-        response = self.guest_client.get('/second_page/')
+        response = self.guest_client.get(reverse('second_page'))
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_page_shows_correct_content(self):
         """Проверка контента страниц."""
-        response = self.guest_client.get('/')
+        response = self.guest_client.get(reverse('index'))
         self.assertContains(response, 'У меня получилось!')
 
-        response = self.guest_client.get('/second_page/')
+        response = self.guest_client.get(reverse('second_page'))
         self.assertContains(response, 'А это вторая страница!')
